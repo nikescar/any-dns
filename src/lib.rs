04cf8c3cd00901a11968ff9f36a0240e1ff74865
Severin Buhler <sevi_buehler@hotmail.com>
2024-02-14 14:30:52 +0100

    expose HandleHolder

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,6 @@ mod pending_request;
 mod query_id_manager;
 mod server;
 
-pub use crate::custom_handler::{CustomHandler, CustomHandlerError, EmptyHandler};
+pub use crate::custom_handler::{CustomHandler, CustomHandlerError, EmptyHandler, HandlerHolder};
 pub use crate::dns_socket::DnsSocket;
 pub use crate::server::{AnyDNS, Builder};
